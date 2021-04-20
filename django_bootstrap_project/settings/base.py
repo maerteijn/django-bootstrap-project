5fbb539de612e908bd49608c923560b5a9d896df
Martijn Jacobs <maerteijn@gmail.com>
2021-04-20 21:07:30 +0200

    Added django-environ

--- django_bootstrap_project/settings/base.py
+++ django_bootstrap_project/settings/base.py
@@ -1,5 +1,11 @@
+import environ
+
+env = environ.Env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "b9n^vq4kazd9%#xipegu@^+dt-4=f72ab3=v8jrjlqgc-dcaul"
+SECRET_KEY = env.str(
+    "SECRET_KEY", default="b9n^vq4kazd9%#xipegu@^+dt-4=f72ab3=v8jrjlqgc-dcaul"
+)
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
