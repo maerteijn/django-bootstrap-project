10dc39b4b438e1d04177b3b0d08d39122a90d169
Martijn Jacobs <maerteijn@gmail.com>
2021-05-20 13:20:49 +0200

    Set django_debug_mode to true so whitenoise won't break

--- tests/functional/test_boilerplate.py
+++ tests/functional/test_boilerplate.py
@@ -1,2 +1,20 @@
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+
 def test_django_bootstrap_project_installed(settings):
     assert hasattr(settings, "DEBUG")
+
+
+@pytest.mark.django_db
+def test_create_user():
+    User.objects.create_user(username="test")
+
+
+@pytest.mark.django_db
+def test_login_django_admin(admin_client):
+    response = admin_client.get(reverse("admin:index"))
+    assert response.status_code == 200
